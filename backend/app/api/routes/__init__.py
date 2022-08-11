e30d89fb03972d786f934f18cd04038f5f156293
kawashow <ksks17179@gmail.com>
2022-08-11 21:36:08 +0900

    #refactor: change router name

--- backend/app/api/routes/__init__.py
+++ backend/app/api/routes/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from app.api.routes.user import router as hedgehogs_router
+from app.api.routes.user import router as user_router
 
 
 router = APIRouter()
-router.include_router(hedgehogs_router, prefix="/user", tags=["user"])
+router.include_router(user_router, prefix="/user", tags=["user"])
